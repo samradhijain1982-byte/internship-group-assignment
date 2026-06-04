f8bcc917be7377ba890bc6e65f9ee020d9f528c1
rishisharma2404-eng <rishisharma2404@gmail.com>
2026-06-04 10:37:28 +0530

    added power and modulus operations

--- calculator.py
+++ calculator.py
@@ -12,5 +12,11 @@ def divide(a,b):
         raise ValueError("Cannot divide by zero")
     return a/b
 
+def power(a,b):
+    return a**b
+
+def modulus(a,b):
+    return a%b
+
 def floor_division(a,b):
     return a//b
